f00c86f2840c88954aa466c219995b32c5fdf0d9
illidar <illidar89@gmail.com>
2014-10-07 19:50:19 +0300

    add test comment

--- app/code/local/Pfay/Test/Block/Monblock.php
+++ app/code/local/Pfay/Test/Block/Monblock.php
@@ -3,6 +3,7 @@ class Pfay_Test_Block_Monblock extends Mage_Core_Block_Template
 {
     public function methodblock()
     {
+        //test comment
         return 'informations about my block !!' ;
      }
 }
